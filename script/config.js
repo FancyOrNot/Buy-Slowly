0ee980896d1e79d6c75c8902aadbe6b3baf25a6b
joy <joy.FancyOrNot.com>
2017-08-21 18:43:19 +0800

    基本布局已完成

---
+++ script/config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2017/8/15.
+ */
+require.config({
+  baseUrl: '/script',
+  paths: {
+    'jquery': 'jquery/jquery',
+    'cookie': 'jquery-cookie/jquery.cookie',
+    'bootstrap': 'bootstrap/js/bootstrap.min',
+    'template': 'artTemplate/template',
+    'nprogress': 'nprogress/nprogress',
+    'form': 'jquery-form/jquery.form',
+    'uploadify': 'uploadify/jquery.uploadify',
+    'region': 'jquery-region/jquery.region',
+    'ckeditor': 'ckeditor/ckeditor',
+    'utils': '/utils',
+    'Jcrop': 'Jcrop/js/Jcrop',
+//      'common': 'js/common'
+    // 'common': 'js/dashboard/common',
+    // 'login': 'js/dashboard/login'
+  },
+  shim: {
+    bootstrap: {
+      deps: ['jquery']
+    },
+    uploadify: {
+      deps: ['jquery']
+    },
+    ckeditor: {
+      exports: 'CKEDITOR'
+    },
+    Jcrop:{
+      deps:['jquery']
+    }
+  }
+});
+require(['common']);
